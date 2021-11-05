e78c58308ced28745b76006f1fc1fc045b4b7a0a
tjansky <tomislav.jansky@gmail.com>
2021-11-05 17:25:15 +0100

    Added references between projects

--- BookShop.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BookShop.Core
-{
-    public class Class1
-    {
-    }
-}
--- BookShop.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BookShop.Services
-{
-    public class Class1
-    {
-    }
-}

5d6692535c449ab6e74bc26664ef0b1362fdd6be
tjansky <tomislav.jansky@gmail.com>
2021-11-09 19:18:06 +0100

    Added checkout module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {path: 'shop', loadChildren: () => import('./modules/shop/shop.module').then(mod => mod.ShopModule)},
   {path: 'account', loadChildren: () => import('./modules/account/account.module').then(mod => mod.AccountModule)},
   {path: 'basket', loadChildren: () => import('./modules/basket/basket.module').then(mod => mod.BasketModule)},
+  {path: 'checkout', loadChildren: () => import('./modules/checkout/checkout.module').then(mod => mod.CheckoutModule), canActivate: [AuthGuard]},
   {path: '**', redirectTo: 'shop', pathMatch: 'full'}
 ];
 

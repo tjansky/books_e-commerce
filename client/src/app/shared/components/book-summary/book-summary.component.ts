57bd1d4c837ee35126f48c7ebeb86169ce5dfaae
tjansky <tomislav.jansky@gmail.com>
2022-01-12 18:23:15 +0100

    Basket updated

--- client/src/app/shared/components/book-summary/book-summary.component.ts
+++ client/src/app/shared/components/book-summary/book-summary.component.ts
@@ -12,26 +12,9 @@ export class BookSummaryComponent implements OnInit {
   //@Input() isBasketElement = false;
   @Input() basketItem: Item = null;
 
-  title: string;
-  format: string;
-  author: string;
-  priceOfItem: number;
-  imageUrl: string;
-
-  quantity: number;
-
   constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
-    if (this.basketItem) {
-      this.priceOfItem = this.basketItem.price
-      this.quantity = this.basketItem.quantity;
-      this.author = "Benjamin Harmon";
-      this.title = this.basketItem.title;
-      this.format = "Paperback";
-      this.imageUrl = this.basketItem.image;
-    }
-    
   }
 
   // basket methods
